fb4d0094a452cb1769edf5ca382aac1805370fdd
Tim Kye <Timothy.Kye@nike.com>
2019-12-24 13:58:46 -0800

    switch spinner

--- client/src/components/PageSpinner/PageSpinner.js
+++ client/src/components/PageSpinner/PageSpinner.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Spinner from 'react-md-spinner'
+import { DualRing } from 'react-loading-io'
 
 import './pageSpinner.css'
 
 export default function PageSpinner() {
   return (
     <div className="page-spinner-container">
-      <Spinner size={256} />
+      <DualRing size={256} />
     </div>
   )
 }
