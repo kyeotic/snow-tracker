bb8316db1ead369c47773fa9e1df6a25a8c90539
Tim Kye <Timothy.Kye@nike.com>
2021-11-21 14:28:14 -0800

    fix: lambda build

--- server/src/lambda/lambda.js
+++ server/src/lambda/lambda.js
@@ -1,6 +1,11 @@
 'use strict'
 
 // eslint-disable-next-line import/extensions
-const { handler } = require('./api')
+// const { handler } = require('../api')
 
-exports.handler = handler
+// exports.handler = handler
+
+exports.handler = async (event, context) => {
+  const { handler } = await import('../api.js')
+  return handler(event, context)
+}
