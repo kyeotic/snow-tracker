997d70904238685551d095e288455c378eb1e08c
Tim Kye <tim@kye.dev>
2023-03-26 13:44:12 -0700

    wip

--- src/remix.gen.ts
+++ src/remix.gen.ts
@@ -2,48 +2,48 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `deno task dev`.
 
-import * as serverEntry from "./app/entry.server.tsx";
-import * as route0 from "./app/root.tsx";
-import * as route1 from "./app/routes/index.tsx";
+import * as serverEntry from './app/entry.server.tsx'
+import * as route0 from './app/root.tsx'
+import * as route1 from './app/routes/index.tsx'
 
-export const entry = { module: serverEntry };
+export const entry = { module: serverEntry }
 export const routes = {
-	"root": {
-		id: "root",
-		module: route0,
-		file: "./app/root.tsx",
-	},
-	"routes/index": {
-		id: "routes/index",
-		index: true,
-		parentId: "root",
-		module: route1,
-		file: "./app/routes/index.tsx",
-	},
-};
+  root: {
+    id: 'root',
+    module: route0,
+    file: './app/root.tsx',
+  },
+  'routes/index': {
+    id: 'routes/index',
+    index: true,
+    parentId: 'root',
+    module: route1,
+    file: './app/routes/index.tsx',
+  },
+}
 export const assets = {
-  entry: { imports: [], module: "/entry.client.js" },
+  entry: { imports: [], module: '/entry.client.js' },
   routes: {
-		"root": {
-			id: "root",
-			imports: [],
-			module: "/root.js",
-			hasAction: "action" in route0,
-			hasLoader: "loader" in route0,
-			hasCatchBoundary: "CatchBoundary" in route0,
-			hasErrorBoundary: "ErrorBoundary" in route0,
-		},
-			"routes/index": {
-			id: "routes/index",
-			index: true,
-			parentId: "root",
-			imports: [],
-			module: "/routes/index.js",
-			hasAction: "action" in route1,
-			hasLoader: "loader" in route1,
-			hasCatchBoundary: "CatchBoundary" in route1,
-			hasErrorBoundary: "ErrorBoundary" in route1,
-		},		
-},
-};
-export const future = {};
+    root: {
+      id: 'root',
+      imports: [],
+      module: '/root.js',
+      hasAction: 'action' in route0,
+      hasLoader: 'loader' in route0,
+      hasCatchBoundary: 'CatchBoundary' in route0,
+      hasErrorBoundary: 'ErrorBoundary' in route0,
+    },
+    'routes/index': {
+      id: 'routes/index',
+      index: true,
+      parentId: 'root',
+      imports: [],
+      module: '/routes/index.js',
+      hasAction: 'action' in route1,
+      hasLoader: 'loader' in route1,
+      hasCatchBoundary: 'CatchBoundary' in route1,
+      hasErrorBoundary: 'ErrorBoundary' in route1,
+    },
+  },
+}
+export const future = {}
